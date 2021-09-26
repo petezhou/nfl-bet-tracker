bc762ffc0ac7db792c2e72e670063443a8e829d6
Peter Zhou <petezhouca@gmail.com>
2021-09-26 16:19:11 -0400

    Strip out React default app elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -10,22 +9,10 @@ function App() {
       setCurrentTime(data.time);
     })
   }, []);
-  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>The current time is {currentTime}</p>
       </header>
     </div>
